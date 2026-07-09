b27a561bf5f077ccd11bee4666143c64129400c0
Karim Hmaidan <khmaidan123@gmail.com>
2026-07-09 20:43:28 +0300

    completed notes CRUD operations

--- app/main.py
+++ app/main.py
@@ -36,4 +36,24 @@ def get_note(note_id: int):
         if note["id"] == note_id :
             return note
     raise HTTPException(status_code = 404)
-        
+
+@app.put("/notes/{note_id}")
+def update_note(note_id: int, updated_note: Note):
+    for note in notes:
+        if note["id"] == note_id:
+            note["title"] = updated_note.title
+            note["content"] = updated_note.content
+            return {"message": "note updated",
+                    "note": note}
+    raise HTTPException(status_code = 404)
+
+@app.delete("/notes/{note_id}")
+def delete_note(note_id: int):
+    for note in notes:
+        if note["id"] == note_id:
+            notes.remove(note)
+            return {"message": "note deleted"}
+    raise HTTPException(status_code = 404, detail="Note not found")
+
+
+
