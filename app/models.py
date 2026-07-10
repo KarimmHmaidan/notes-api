fd938afcc0f80fb962e02e256fcafcab09abf2af
Karim Hmaidan <khmaidan123@gmail.com>
2026-07-10 17:23:25 +0300

    added user model and database migration

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime
 from app.database import Base
+from datetime import datetime
 
 
 class Note(Base):
@@ -8,3 +9,13 @@ class Note(Base):
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String)
     content = Column(String)
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
